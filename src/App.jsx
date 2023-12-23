3a0695d976b17840fa4fd787a086accad81e35ad
unknown <sabhyaapradhan2005@gmail.com>
2023-12-23 09:31:38 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,46 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
 const App = () => {
   const quotes = [
     'A rose by any other name would smell as sweet.',
     'All that glitters is not gold',
-    'All the worlds a stage, and all the men and wmen merely players.'
-  ]
+    'All the worlds a stage, and all the men and women merely players.'
+  ];
 
-  const [points, setPoints] = useState({ 0: 1, 1: 3, 2: 4})
-  const [selected, setSelected] = useState(0)
-  const [quoteoftheday, setQuoteOfTheDay] =useState(0)
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState([0, 0, 0, 0, 0, 0])
 
-  const Vote= () => {
-  const copy = {...points}
-  copy[selected] += 1
-  setPoints(copy)
+  const highest = Math.max(...points)
+  const highestldx = points.indexOf(highest)
 
-  if (copy[selected] > points[quoteoftheday]){
-    setQuoteOfTheDay(selected)
-  }
-}
+  const nextQuote = () => {
+    if (selected === quotes.length - 1) {
+      setSelected(0);
+    } else {
+      setSelected(selected + 1);
+    }
+  };
 
-  const Next = () =>{
-    setSelected(preSelected => preSelected+1)
+  const vote = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
   }
 
   return (
     <div>
       <h1>Quote of the Day</h1>
-      <p>{quotes[quoteoftheday]}</p>
-      <p>has {points[selected]} votes</p>
+      <p>{quotes[highestldx]}</p>
+      <p>has {points[highestldx]} votes</p>
+      <button onClick={vote}>Vote</button>
+      <button onClick={nextQuote}>Next Quote</button>
       <h1>Quotes</h1>
       <p>{quotes[selected]}</p>
       <p>has {points[selected]} votes</p>
-      <button onClick={Next} disabled={selected == 2}>Next</button>
-      <button onClick={Vote}>Vote</button>
+      <button onClick={vote}>Vote</button>
+      <button onClick={nextQuote}>Next Quote</button>
     </div>
   )
 }
-export default App
+
+export default App;
